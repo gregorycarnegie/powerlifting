79400022c53a137dd0f4ec452e111ba9302515d2
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2024-09-29 18:01:17 +0100

    split process_data function into smaller functions

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-import matplotlib.pyplot as plt
 import utils as ut
+from pathlib import Path
 
 
 if __name__ == '__main__':
-    data = "data/openpowerlifting-2024-09-21-f5f73164.csv"
+    data = Path("data/powerlifting_data.csv")
     
-    lift_values, user_total, percentile, user_event, user_sex, compare_within_weight_class, user_weight_class = ut.process_data(data)
+    parameters = ut.process_data(data)
 
-    plt.figure(figsize=(10, 6))
-    plt.hist(lift_values, bins=50, alpha=0.7, label='Lifters')
-    plt.axvline(user_total, color='red', linestyle='dashed', linewidth=2, label='Your Lift')
-    plt.annotate(f'Your Percentile: {percentile:.2f}%', xy=(user_total, plt.ylim()[1]*0.9),
-                 xytext=(user_total, plt.ylim()[1]*0.9), arrowprops=dict(facecolor='red', shrink=0.05),
-                 fontsize=12, color='red', ha='center')
-
-    plt.xlabel(f'{user_event} Total (kg)')
-    plt.ylabel('Number of Lifters')
-    title = f'Distribution of {user_event} Total for {user_sex} lifters'
-    if compare_within_weight_class == 'y':
-        title += f' in Weight Class {user_weight_class}'
-    else:
-        title += ' across all weight classes'
-    plt.title(title)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    ut.plot_histogram(*parameters)
--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 import polars as pl
+import numpy.typing as npt
+from typing import Literal, Union
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+SEX = Literal['M', 'F']
+COMPARE_WEIGHT_CLASS = Literal['Y', 'N']
 
 EVENTS: dict[str, tuple[str, ...]] = {
     'SBD': ('Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg'),
@@ -39,8 +46,9 @@ female_weight_classes: list[tuple[int, int, str]] = [
     (90, 1000, '90 kg+')
 ]
 
+
 # Function to create the weight class assignment expression
-def get_weight_class_expr() -> pl.Expr:
+def custom_weight_class_expr() -> pl.Expr:
     conditions = []
     # For male weight classes
     for min_wt, max_wt, label in male_weight_classes:
@@ -64,8 +72,9 @@ def get_weight_class_expr() -> pl.Expr:
     expr = expr.otherwise(pl.lit('Unknown')).alias('CustomWeightClass')
     return expr
 
+
 # Assign custom weight classes based on 'BodyweightKg' and 'Sex'
-def assign_user_weight_class(sex: str, bw: float) -> str:
+def assign_user_weight_class(sex: SEX, bw: float) -> str:
     weight_classes = male_weight_classes if sex == 'M' else female_weight_classes
     return next(
         (
@@ -76,6 +85,7 @@ def assign_user_weight_class(sex: str, bw: float) -> str:
         'Unknown',
     )
 
+
 def get_float_input(prompt: str) -> float:
     while True:
         try:
@@ -83,19 +93,16 @@ def get_float_input(prompt: str) -> float:
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-def process_data(data: str):
+
+def get_schema_overrides(data:Path) -> dict[str, Union[pl.Float64, pl.String]]:
     with open(data, 'r') as f:
         header = f.readline()
     column_names = header.strip().split(',')
-    kg_columns = [col for col in column_names if col.endswith('Kg')]
-    schema_overrides = {col: (pl.Float64 if col != 'WeightClassKg' else pl.String) for col in kg_columns}
-    df = pl.read_csv(data, schema_overrides=schema_overrides)
-    
-    df = df.filter(pl.col('BodyweightKg').is_not_null() & (pl.col('BodyweightKg') > 0))
-    df = df.filter(pl.col('Sex').is_in(['M', 'F']))
-    df = df.with_columns(get_weight_class_expr())
-    df = df.filter(pl.col('CustomWeightClass') != 'Unknown')
+    kg_columns = (col for col in column_names if col.endswith('Kg'))
+    return {col: (pl.Float64 if col != 'WeightClassKg' else pl.String) for col in kg_columns}
 
+
+def get_lifter_info() -> tuple[str, str, str]:
     user_sex = input("Enter your sex (M or F): ").strip().upper()
     if user_sex not in {'M', 'F'}:
         print("Invalid sex entered.")
@@ -108,11 +115,49 @@ def process_data(data: str):
 
     user_bodyweight = get_float_input("Enter your body weight in kg: ")
     user_weight_class = assign_user_weight_class(user_sex, user_bodyweight)
+
+    return user_sex, user_event, user_weight_class
+
+
+def get_filter_expr(user_sex: SEX,
+                   user_event: str, 
+                   user_weight_class: str, 
+                   relevant_columns: tuple[str, ...], 
+                   compare_within_weight_class: COMPARE_WEIGHT_CLASS) -> pl.Expr:
+    match compare_within_weight_class:
+        case 'Y':
+            return (
+                pl.col('BodyweightKg').is_not_null() & 
+                (pl.col('BodyweightKg') > 0) &
+                (pl.col('Sex') == user_sex) &
+                (pl.col('Event') == user_event) &
+                (pl.col('CustomWeightClass') == user_weight_class) &
+                pl.struct(relevant_columns).is_not_null()
+            )
+        case 'N':
+            return (
+                pl.col('BodyweightKg').is_not_null() & 
+                (pl.col('BodyweightKg') > 0) &
+                (pl.col('Sex') == user_sex) &
+                (pl.col('Event') == user_event) &
+                pl.struct(relevant_columns).is_not_null()
+            )
+
+
+def process_data(data: Path) -> tuple[npt.NDArray, Union[float, Literal[0]], float, str, str, str, str]:
+    schema_overrides = get_schema_overrides(data)
+
+    df = pl.read_csv(data, schema_overrides=schema_overrides)
+    
+    df = df.with_columns(custom_weight_class_expr())
+
+    user_sex, user_event, user_weight_class = get_lifter_info()
+
     if user_weight_class == 'Unknown':
         print("No matching weight class found for your body weight.")
         exit()
 
-    compare_within_weight_class = input("Do you want to compare your lifts within your weight class? (yes/no): ").strip().lower()[0]
+    compare_within_weight_class = input("Do you want to compare your lifts within your weight class? (yes/no): ").strip().upper()[0]
     relevant_columns = EVENTS.get(user_event)
 
     if not relevant_columns:
@@ -120,23 +165,11 @@ def process_data(data: str):
         exit()
 
     # If the user wants to compare within their weight class, add the weight class filter
-    if compare_within_weight_class == 'y':
-        df_filtered = df.filter(
-            (pl.col('Sex') == user_sex) &
-            (pl.col('Event') == user_event) &
-            (pl.col('CustomWeightClass') == user_weight_class) &
-            pl.struct(relevant_columns).is_not_null()
-        ).with_columns(
-            pl.sum_horizontal([pl.col(col) for col in relevant_columns]).alias('Total')
-        )
-    else:
-        df_filtered = df.filter(
-            (pl.col('Sex') == user_sex) &
-            (pl.col('Event') == user_event) &
-            pl.struct(relevant_columns).is_not_null()
-        ).with_columns(
-            pl.sum_horizontal([pl.col(col) for col in relevant_columns]).alias('Total')
-        )
+    df_filtered = df.filter(
+        get_filter_expr(user_sex, user_event, user_weight_class, relevant_columns, compare_within_weight_class)
+    ).with_columns(
+        pl.sum_horizontal(pl.col(col) for col in relevant_columns).alias('Total')
+    )
 
     lift_values = df_filtered['Total'].to_numpy()
     lift_values = lift_values[lift_values > 0]
@@ -147,3 +180,30 @@ def process_data(data: str):
     percentile = df_filtered.filter(pl.col('Total') < user_total).shape[0] / df_filtered.shape[0] * 100
 
     return lift_values, user_total, percentile, user_event, user_sex, compare_within_weight_class, user_weight_class
+
+
+def plot_histogram(lift_values: npt.NDArray, 
+                   user_total: Union[float, Literal[0]], 
+                   percentile: float, 
+                   user_event: str, 
+                   user_sex: str, 
+                   compare_within_weight_class: str, 
+                   user_weight_class: str) -> None:
+    plt.figure(figsize=(10, 6))
+    plt.hist(lift_values, bins=50, alpha=0.7, label='Lifters')
+    plt.axvline(user_total, color='red', linestyle='dashed', linewidth=2, label='Your Lift')
+    plt.annotate(f'Your Percentile: {percentile:.2f}%', xy=(user_total, plt.ylim()[1]*0.9),
+                 xytext=(user_total, plt.ylim()[1]*0.9), arrowprops=dict(facecolor='red', shrink=0.05),
+                 fontsize=12, color='red', ha='center')
+
+    plt.xlabel(f'{user_event} Total (kg)')
+    plt.ylabel('Number of Lifters')
+    title = f'Distribution of {user_event} Total for {user_sex} lifters'
+    if compare_within_weight_class == 'Y':
+        title += f' in Weight Class {user_weight_class}'
+    else:
+        title += ' across all weight classes'
+    plt.title(title)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
